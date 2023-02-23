be0969deca0c1d58272e794c471687fecb193e29
Zink Pavel <tsynk.p@gmail.com>
2023-02-23 22:25:03 +0700

    Add 1.4.1 exam

--- _1___Base/_4__Namespaces_and_scopes/_1_namespaces/namespaces.py
+++ _1___Base/_4__Namespaces_and_scopes/_1_namespaces/namespaces.py
@@ -1,9 +1,97 @@
+from enum import Enum
 from typing import Self
 
 
 class Namespace:
-    def __init__(self, name: str, parent: Self) -> None:
+    def __init__(
+        self: Self,
+        name: str = None,
+        parent: Self = None,
+        sibling: Self = None,
+        variables: list[str] = None,
+    ) -> None:
         self.name = name
         self.parent = parent
-        self.siblings = None
-        self.variables = None
+        self.sibling = sibling
+        self.variables = variables or list()
+
+    def __str__(self):
+        string = f"""
+{self.name=}
+    {self.parent=}
+    {self.sibling=}
+    {self.variables=}
+"""
+        return string
+
+
+class Command(Enum):
+    ADD = "add"
+    CREATE = "create"
+    GET = "get"
+
+
+class Programm:
+    def __init__(
+        self: Self,
+        namespaces: dict[str:Namespace] = None,
+    ):
+        self.namespaces = namespaces or dict()
+        if not self.namespaces or "global" not in self.namespaces:
+            self.namespaces.update(
+                {"global": Namespace(name="global", parent=Namespace(name=None))}
+            )
+
+    def __str__(self):
+        string = "\n--- Start ---"
+        for elem in self.namespaces.values():
+            string += str(elem)
+        return string + "--- Finish ---"
+
+    def execute(self: Self, command: str):
+        args = command.split(" ")
+        _command = args[0]
+        if _command == Command.ADD.value:
+            self.__add_variable_to_namespace(*args[1:])
+        elif _command == Command.CREATE.value:
+            self.__create_new_namespace(*args[1:])
+        elif _command == Command.GET.value:
+            return self.__get_namespace_containing_var(*args[1:])
+        else:
+            raise (RuntimeError(f"Command {_command} is not available."))
+
+    def __add_variable_to_namespace(self: Self, namespace: str, variable: str):
+        if self.namespaces.get(namespace) is None:
+            self.namespaces.update(
+                {namespace: Namespace(name=namespace, variables=[variable])}
+            )
+        else:
+            self.namespaces[namespace].variables.append(variable)
+        #     raise (RuntimeError(f"Namespace {namespace} is not defined."))
+
+    def __create_new_namespace(self: Self, namespace: str, parent: str):
+        _namespace = self.namespaces.get(namespace)
+        if _namespace:
+            return
+            # raise (RuntimeError(f"Namespace {namespace} already exists."))
+        new_namespace = Namespace(name=namespace, parent=self.namespaces.get(parent))
+        self.namespaces.update({namespace: new_namespace})
+        self.namespaces.get(parent).sibling = new_namespace
+
+    def __get_namespace_containing_var(
+        self: Self, namespace: Namespace, variable: str
+    ) -> str:
+        _namespace = self.namespaces.get(namespace)
+        if _namespace is None:
+            return None
+        if variable in _namespace.variables:
+            return _namespace.name
+        else:
+            return self.__get_namespace_containing_var(_namespace.parent.name, variable)
+
+
+if __name__ == "__main__":
+    prog = Programm()
+    n = int(input())
+    for _ in range(n):
+        prog.execute(input())
--- _1___Base/_4__Namespaces_and_scopes/_1_namespaces/test.py
+++ _1___Base/_4__Namespaces_and_scopes/_1_namespaces/test.py
@@ -1,11 +1,36 @@
 import unittest
 
-from .namespaces import *
+from .namespaces import Programm
 
 
 class TestNamespaces(unittest.TestCase):
-    def test(self):
-        pass
+    def setUp(self):
+        self.prog = Programm()
+
+    def test_1(self):
+        self.prog.execute("add global a")
+        self.prog.execute("create foo global")
+        self.prog.execute("add foo b")
+        self.prog.execute("create bar foo")
+        self.prog.execute("add bar a")
+
+        self.assertEqual(self.prog.namespaces["global"].variables, ["a"])
+        self.assertEqual(self.prog.namespaces["foo"].variables, ["b"])
+        self.assertEqual(self.prog.namespaces["bar"].variables, ["a"])
+
+    def test_2(self):
+        self.prog.execute("add global a")
+        self.prog.execute("create foo global")
+        self.prog.execute("add foo b")
+
+        self.assertEqual(self.prog.execute("get foo a"), "global")
+        self.assertEqual(self.prog.execute("get foo c"), None)
+
+        self.prog.execute("create bar foo")
+        self.prog.execute("add bar a")
+
+        self.assertEqual(self.prog.execute("get bar a"), "bar")
+        self.assertEqual(self.prog.execute("get bar b"), "foo")
 
 
 if __name__ == "__main__":
