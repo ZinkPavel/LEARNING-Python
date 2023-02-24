8b87729fe99bec0f3023156a7cc047fecc1cdc01
Zink Pavel <tsynk.p@gmail.com>
2023-02-24 18:53:56 +0700

    Add 1.6.3 LoggableList

---
+++ _1___Base/_6__Inheritance/_3_loggable_list/loggable_list.py
@@ -0,0 +1,12 @@
+import time
+
+
+class Loggable:
+    def log(self, msg):
+        print(str(time.ctime()) + ": " + str(msg))
+
+
+class LoggableList(list, Loggable):
+    def append(self, elem):
+        super().append(elem)
+        self.log(elem)
